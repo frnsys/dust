5ed4090e7f0afc5d5832c5f24933ad719c3013c8
Francis Tseng <f+accounts@frnsys.com>
2022-01-22 15:52:07 -0500

    support for different pattern files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod progression;
 mod audio;
 mod midi;
 
-use std::fs::File;
+use clap::{Parser, ValueHint};
+use std::{fs::File, path::PathBuf};
 use std::io::BufReader;
 use std::io;
 use app::{App, run_app};
@@ -20,9 +21,17 @@ use tui::{
 };
 use progression::ProgressionTemplate;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Root
+    #[clap(short, long, default_value = "patterns.yaml", value_hint = ValueHint::FilePath)]
+    patterns: PathBuf,
+}
 
 fn main() -> Result<()> {
-    let file = File::open("patterns.yaml").expect("could not open file");
+    let args = Args::parse();
+    let file = File::open(args.patterns).expect("could not open file");
     let reader = BufReader::new(file);
     let template: ProgressionTemplate = serde_yaml::from_reader(reader).expect("error while reading yaml");
 
